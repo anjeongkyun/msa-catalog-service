1549181cc35c1a004c0a635503b947e7bd957934
dkswjdrbs12@naver.com <dkswjdrbs12@naver.com>
2022-10-24 00:17:10 +0900

    fix: insert column name 수정 (product -> product_id)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into catalog(product, product_name, stock, unit_price)
+insert into catalog (product_id, product_name, stock, unit_price)
 values ('CATALOG-001', 'Berlin', 100, 1500);
 
-insert into catalog(product, product_name, stock, unit_price)
+insert into catalog (product_id, product_name, stock, unit_price)
 values ('CATALOG-002', 'Tokyo', 130, 1000);
 
-insert into catalog(product, product_name, stock, unit_price)
+insert into catalog (product_id, product_name, stock, unit_price)
 values ('CATALOG-003', 'Stockholm', 200, 2000);
